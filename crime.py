a6700d1011e9e3f710138ddd4d744526c30b36cc
Kayann Smith <ksmith@ucvts.org>
2017-01-23 15:44:24 +0000

    Revised bargraph

--- crime.py
+++ crime.py
@@ -9,9 +9,14 @@ df = pd.read_csv('crimedata.csv')
 #import needed plotting imports
 from bokeh.charts import Bar, output_file, save
 
+states = []
+for n in range(len(df)):
+    name = df.iloc[n]['county_name']
+    state = name[-2:]
+    states.append(state)
 
 # To set up the bar graph 
-p = Bar(df, label='County Names', values='ROBBERY', title='# of Robberies in different areas', color='green')
+p = Bar(df, label='county_name', values='ROBBERY', title='# of Robberies in different areas', color='green')
 
 # create an output file
 output_file('bargraph.html')
